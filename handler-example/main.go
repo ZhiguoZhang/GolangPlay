c58e15df6383cc68964aa6474a9f0839c92bcf6f
Zhiguo Zhang <zhiguo.zhang@foodora.com>
2017-07-28 23:15:34 +0200

    Add a handler-example

---
+++ handler-example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	rh := http.RedirectHandler("https://docs.google.com/spreadsheets/d/1gCxRH7Qs3Hoi9XIYKtJqwK32pw7xtFB6BBsSu7rjQ0c/edit?usp=sharing", 307)
+
+	mux.Handle("/statistic", rh)
+
+	log.Println("Listening...")
+
+	http.ListenAndServe(":3000", mux)
+}
+
+
+
+
+
