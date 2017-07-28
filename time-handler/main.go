4e1109e700e847eb682e2b8fcc77097716fca696
Zhiguo Zhang <zhiguo.zhang@foodora.com>
2017-07-28 23:54:37 +0200

    Use go format

--- time-handler/main.go
+++ time-handler/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "time"
+	"log"
+	"net/http"
+	"time"
 )
 
 //Code example for http://www.alexedwards.net/blog/a-recap-of-request-handling
 
 func timeHandler(format string) http.Handler {
-    fn := func(w http.ResponseWriter, r *http.Request) {
-        tm := time.Now().Format(format)
-        w.Write([]byte("The time is: " + tm))
-    }
-    return http.HandlerFunc(fn)
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		tm := time.Now().Format(format)
+		w.Write([]byte("The time is: " + tm))
+	}
+	return http.HandlerFunc(fn)
 }
 
 func main() {
 
+	th1123 := timeHandler(time.RFC1123)
 
-    th1123 := timeHandler(time.RFC1123)
+	http.Handle("/time/rfc1123", th1123)
 
-    http.Handle("/time/rfc1123", th1123)
+	th3339 := timeHandler(time.RFC3339)
 
-    th3339 := timeHandler(time.RFC3339)
+	http.Handle("/time/rfc3339", th3339)
 
-    http.Handle("/time/rfc3339", th3339)
-
-    log.Println("Listening.....")
-    http.ListenAndServe(":3000", nil)
+	log.Println("Listening.....")
+	http.ListenAndServe(":3000", nil)
 }
