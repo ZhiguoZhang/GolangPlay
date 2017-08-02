7a4e0268d7229039029297b75dc5f8031ab4c25e
mikewolfxyou <zhiguo.zhang@foodora.com>
2017-08-02 14:44:58 +0200

    Use http handler redirect

---
+++ 06_constants/01_constant/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const p = "Dead & taxes"
+const q = 42
+
+func main() {
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+}
--- redirect-middleware/main.go
+++ redirect-middleware/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"github.com/ScruffyProdigy/Middleware/redirecter"
-	"github.com/ScruffyProdigy/TheRack/httper"
+	"net/http"
 )
 
 func main() {
-	conn := httper.HttpConnection(":3000")
-	conn.Go(redirecter.Redirecter{Path: "https://docs.google.com/forms/d/e/1FAIpQLSe3sMjvlpw90JlIwEDFGmZACnPDXRjA8KGDbKGY3xjBsvYRig/viewform?usp=sf_link"})
+	http.Handle("/", http.HandlerFunc(redirect))
+	http.ListenAndServe(":3000", nil)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+
+	//TODO get path -> url map from DB/Redis
+	switch r.URL.Path {
+	case "/statistic":
+		http.Redirect(w, r, "https://docs.google.com/forms/d/e/1FAIpQLSe3sMjvlpw90JlIwEDFGmZACnPDXRjA8KGDbKGY3xjBsvYRig/viewform", 307)
+	case "/statistic-result":
+		http.Redirect(w, r, "https://docs.google.com/spreadsheets/d/1gCxRH7Qs3Hoi9XIYKtJqwK32pw7xtFB6BBsSu7rjQ0c/edit#gid=1007446279", 307)
+	}
 }
